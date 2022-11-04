bf144dfbb42f0bda56b1d0c275ce18a26e819080
kravninja <tita0@chicagobooth.edu>
2022-11-04 23:53:33 +0000

    Updated movie template

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,10 +9,10 @@ class MoviesController < ApplicationController
   def movie_details
     # params looks like  Parameters: {"id"=>"movies"}
     @the_movie_id = params.fetch("id")
-    
+    the_id = params.fetch("id")
     @the_director = Director.where({:id => @the_movie_id}).at(0)
     @filmography = Movie.where({:director_id => @the_movie_id})
-    #@movie_detail = Movie.where({:title =>})
+    @the_movie = Movie.where({:id => the_id}).at(0)
     #Movie.all.at(0).title
     render ({:template => "movies_template/movie_details.html.erb"})
   end
