457d621909e24f224a12d3cd75ee7f9d3b31c55b
kravninja <tita0@chicagobooth.edu>
2022-11-04 13:49:46 +0000

    more movie stuff

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,10 +8,10 @@ class MoviesController < ApplicationController
 
   def movie_details
     # params looks like  Parameters: {"id"=>"movies"}
-    the_movie_id = params.fetch("id")
+    @the_movie_id = params.fetch("id")
     
-    @the_director = Director.where({:id => the_movie_id}).at(0)
-    @filmography = Movie.where({:director_id => the_movie_id})
+    @the_director = Director.where({:id => @the_movie_id}).at(0)
+    @filmography = Movie.where({:director_id => @the_movie_id})
 
     render ({:template => "movies_template/movie_details.html.erb"})
   end
